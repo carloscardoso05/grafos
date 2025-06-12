4e7119c005b1b1ce3e642ac0a2fb17be89e33197
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2025-06-12 11:31:47 -0300

    update

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,11 @@
+import grafo.Main;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class MainTest {
-    @Test
-    void testMain() {
-    }
+	@Test
+	void testMain() {
+		assertDoesNotThrow(() -> Main.main(null), "Main method should run without exceptions");
+	}
 }
